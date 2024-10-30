0040465587873f0aca2a2a77b1efcce6d6f50a75
Timur <64714613+Akhmadulin@users.noreply.github.com>
2024-10-30 14:28:48 +0500

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -8,9 +8,9 @@ if [ ! -f "$CONFIG_FILE" ]; then
 fi
 
 # === Извлечение значений из config.json ===
-ORG_ID=$(grep -oP '(?<="id": ")[^"]*' $CONFIG_FILE)
-PASSWORD=$(grep -oP '(?<="password": ")[^"]*' $CONFIG_FILE)
-BINCODES=$(grep -oP '(?<="bincodes": \[)[^\]]*' $CONFIG_FILE | tr -d '"' | tr ',' ' ')
+ORG_ID=$(grep -oP '(?<="id": ")[^"]*' "$CONFIG_FILE")
+PASSWORD=$(grep -oP '(?<="password": ")[^"]*' "$CONFIG_FILE")
+BINCODES=$(grep -oP '(?<="bincodes": \[)[^\]]*' "$CONFIG_FILE" | tr -d '"' | tr ',' ' ')
 
 # === Генерация случайной соли из 16 символов ===
 ORG_SALT=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
@@ -52,12 +52,32 @@ fi
 # Создаем обновленную версию private.yaml с замененными значениями
 sed -i \
   -e "s|<org>|$ORG_ID|g" \
+  -e "s|<group>|$ORG_ID|g" \
   -e "s|<org-signing.key password>|$PASSWORD|g" \
   -e "s|<org-signing.key secret>|$PASSWORD|g" \
   -e "s|<org-encryption.key secret>|$PASSWORD|g" \
   -e "s|<group-encryption.key password>|$PASSWORD|g" \
   -e "s|<org secret salt>|$ORG_SALT|g" \
   "$PRIVATE_YAML"
+
+# === Проверка и добавление строки в config.yaml ===
+CONFIG_YAML="agent/config.yaml"
+if [ -f "$CONFIG_YAML" ]; then
+  if ! grep -q "id: $ORG_ID" "$CONFIG_YAML"; then
+    echo "Строка с id: $ORG_ID не найдена. Добавляем в config.yaml..."
+    cat <<EOF >> "$CONFIG_YAML"
+
+    - id: $ORG_ID
+      title: $ORG_ID
+      signing-certificate-file: certs/${ORG_ID}-signing.crt
+EOF
+  else
+    echo "Строка с id: $ORG_ID уже присутствует в config.yaml."
+  fi
+else
+  echo "Файл config.yaml не найден!"
+  exit 1
+fi
 
 # === Переход в каталог agent ===
 cd agent || { echo "Каталог agent не найден!"; exit 1; }
